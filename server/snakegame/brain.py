017970e73e9eb10999f6c116530876043c134c36
firatkizilboga <firatkizilboga11@gmail.com>
2023-02-25 11:05:04 +0300

    removed the bias

--- server/snakegame/brain.py
+++ server/snakegame/brain.py
@@ -3,18 +3,14 @@ import numpy as np
 class Brain():
     def __init__(self):
         self.weights = [np.random.rand(9, 3), #input layer
-                        np.random.rand(3,3)
+                        np.random.rand(3,3), #output layer
                         ]
-        self.bias = [
-                    np.random.rand(3),
-                    np.random.rand(3),
-                    ]
     
     def predict(self, inputs):
         #multiply inputs by weights and add bias for every layer
         xw=np.array(inputs).ravel()
-        for weights, bias in zip(self.weights, self.bias):
-            xw = np.dot(xw,weights) + bias
+        for weights in self.weights:
+            xw = np.dot(xw,weights)
         xw=xw.reshape(-1,1).tolist()
         #make the biggest value 1 and the rest 0
         xw[xw.index(max(xw))]=1
@@ -55,12 +51,6 @@ class Brain():
                 for i in range(len(row)):
                     if np.random.rand() < mutation_rate:
                         row[i] += np.random.rand()
-        
-        for layer in bias:
-            for i in range(len(layer)):
-                if np.random.rand() < mutation_rate:
-                    layer[i] += np.random.rand()
-
         #create a new brain
         brain = Brain()
         brain.weights = weights
